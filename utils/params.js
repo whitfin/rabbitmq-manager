44a4cb65124526f970fa5b638359364ba3599195
Isaac Whitfield <iwhitfield@appcelerator.com>
2014-07-08 18:26:46 -0700

    Better formatting, now using spaces

--- utils/params.js
+++ utils/params.js
@@ -1,29 +1,29 @@
 var args = require('minimist')(process.argv.slice(3)),
-	config = require('./config');
+    config = require('./config');
 
 var conf = args.c || args.conf || args.config,
-	poll = args.poll || false;
+    poll = args.poll || false;
 
 var pass = args.p || args.pass || args.password || config[conf] ? config[conf].password : undefined,
-	user = args.u || args.user || args.username || config[conf] ? config[conf].username : undefined;
+    user = args.u || args.user || args.username || config[conf] ? config[conf].username : undefined;
 
 if((!user || (!pass && !~user.indexOf(':'))) && !conf){
-	console.log('\nPlease enter a valid username and password.\n');
-	process.exit(1);
+    console.log('\nPlease enter a valid username and password.\n');
+    process.exit(1);
 }
 
 if(conf && !config[conf]){
-	console.log('\nPlease ensure to setup a configuration for ' + conf + '.\n');
-	process.exit(1);
+    console.log('\nPlease ensure to setup a configuration for ' + conf + '.\n');
+    process.exit(1);
 }
 
 var host = args['rabbit-host'] || config[conf || 'default'].host,
-	port = args['rabbit-port'] || config[conf || 'default'].port;
+    port = args['rabbit-port'] || config[conf || 'default'].port;
 
 module.exports = {
-	host:host,
-	pass:pass,
-	poll:poll,
-	port:port,
-	user:user
+    host:host,
+    pass:pass,
+    poll:poll,
+    port:port,
+    user:user
 };
--- utils/schema.js
+++ utils/schema.js
@@ -1,20 +1,22 @@
 var joi = require('joi');
 
 var schema = {
-	'host': joi.string().hostname(),
-	'port': joi.string().regex(/\d/),
-	'username': joi.string().min(1),
-	'password': joi.string().min(1)
+    'host': joi.string().hostname(),
+    'port': joi.string().regex(/\d/),
+    'username': joi.string().min(1),
+    'password': joi.string().min(1)
 };
 
 module.exports = function(config){
-	for(var key in config) {
-		var v = joi.validate(config[key], schema);
-		if (v.error) {
-			console.log('\nUnable to verify config entry \'' + key + '\'!\n\n' + v.error.stack);
-			process.exit(1);
-		}
-		config[key] = v.value;
-	}
-	return config.sort(true, true);
+    for(var key in config) {
+        if(config.hasOwnProperty(key)){
+            var v = joi.validate(config[key], schema);
+            if (v.error) {
+                console.log('\nUnable to verify config entry \'' + key + '\'!\n\n' + v.error.stack);
+                process.exit(1);
+            }
+            config[key] = v.value;
+        }
+    }
+    return config.sort(true, true);
 };
